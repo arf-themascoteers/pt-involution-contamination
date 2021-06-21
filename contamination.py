516eec982e73627145f222c8434369ac3076602d
arif <arif@bitmascot.com>
2021-06-21 17:04:00 +1000

    Done

--- contamination.py
+++ contamination.py
@@ -18,24 +18,47 @@ class Contamination(nn.Module):
         y = x.clone()
         for i in range(self.rows):
             for j in range(self.cols):
-                self.contaminate_from_source(x,y,i,j)
+                self.get_contaminated(x, y, i, j)
         return y
 
-    def contaminate_from_source(self, x, y, src_row , src_col):
+    def get_contaminated(self, x, y, dest_row, dest_col):
         drows = [-1,-1,0,1,1,1,0,-1]
         dcols = [0,-1,-1,-1,0,1,1,1]
-
+        neighbours = []
         for i in range(8):
-            neighbour_row = src_row + drows[i]
-            neighbour_col = src_col + dcols[i]
+            neighbour_row = dest_row + drows[i]
+            neighbour_col = dest_col + dcols[i]
 
             if neighbour_row < 0 or neighbour_row >= self.rows:
                 continue
 
             if neighbour_col < 0 or neighbour_col >= self.cols:
                 continue
+            neighbours.append((neighbour_row, neighbour_col))
 
-            self.contaminate_from_source_to_dest(x,y,src_row, src_col, neighbour_row, neighbour_col)
+        self.get_contaminate_by_neighbours(x, y, dest_row, dest_col, neighbours)
 
-    def contaminate_from_source_to_dest(self, x,y, src_row, src_col, dest_row, dest_col):
-        y[:,:,dest_row, dest_col] = y[:,:,dest_row, dest_col] + x[:,:,src_row, src_col] * 0.2
+    def get_contaminate_by_neighbours(self, x, y, dest_row, dest_col, neighbours):
+        count = len(neighbours)
+        if count == 0:
+            return
+
+        summed = torch.zeros([y.shape[0],y.shape[1], self.kernel_size, self.kernel_size])
+        for tup in neighbours:
+            row = tup[0]
+            col = tup[1]
+            row_start = row * self.kernel_size
+            row_end = row * self.kernel_size + self.kernel_size
+            col_start = col * self.kernel_size
+            col_end = col * self.kernel_size + self.kernel_size
+            summed = summed[:,:] + x[:,:,row_start:row_end, col_start:col_end]
+        summed = summed / count
+
+        dest_row_start = dest_row * self.kernel_size
+        dest_row_end = dest_row * self.kernel_size + self.kernel_size
+
+        dest_col_start = dest_col * self.kernel_size
+        dest_col_end = dest_col * self.kernel_size + self.kernel_size
+
+        y[:,:,dest_row_start:dest_row_end, dest_col_start :dest_col_end] = \
+            x[:,:,dest_row_start : dest_row_end, dest_col_start: dest_col_end] * 0.8 + summed[:,:] * 0.2
